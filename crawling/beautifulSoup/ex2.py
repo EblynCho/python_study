722b8634e8f6f215742595213892098b01e34c63
eblyncho <eblyn52@naver.com>
2022-12-05 16:11:34 +0900

    Web Crawling

---
+++ crawling/beautifulSoup/ex2.py
@@ -0,0 +1,12 @@
+# ex2.py
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+
+html = urlopen('https://product.kyobobook.co.kr/bestseller/total')
+
+soup = bs(html.read(), 'html.parser')
+
+book_page_urls = []
+for i in soup.find_all('div', {'class':'sub_title_wrap'}) :
+    print(i.text)
